5da68c70bcda5cdadb7ce0ad2c1d642937cb378a
Somesh Pathak <pathaksomesh@themavericklabs.com>
2025-10-16 12:33:54 +0200

    Initial Commit

---
+++ MAU-Log-Reader/MAU_Log_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAU_Log_ReaderApp.swift
+//  MAU-Log-Reader
+//
+//  Created by Somesh Pathak on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MAU_Log_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MAU-Log-ReaderTests/MAU_Log_ReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MAU_Log_ReaderTests.swift
+//  MAU-Log-ReaderTests
+//
+//  Created by Somesh Pathak on 16/10/2025.
+//
+
+import Testing
+@testable import MAU_Log_Reader
+
+struct MAU_Log_ReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
